7ec4e0abc58c087570901ff17cbd3c37de39daf2
Clairerang <s10223223@connect.np.edu.sg>
2023-04-22 00:23:01 +0800

    test 3

--- Question2.java
+++ Question2.java
@@ -4,17 +4,11 @@ public class Question2
 {
   public static void main(String[] args)
   {
-    /**
-     * Prompts user for height and weight, calculate and print out the BMI of the user
-     * 
-     * Hint: in.nextDouble() to read double
-     */
-     
     Scanner in = new Scanner(System.in);
 
     System.out.print("Enter your weight in kg: ");
 
-    double weight = scanner.nextDouble();
+    double weight = in.nextDouble();
 
     System.out.print("Enter your height in meters: ");
 
@@ -22,6 +16,6 @@ public class Question2
 
     double bmi = weight / (height * height);
 
-    System.out.print("Your BMI is .2f", bmi);
+    System.out.printf("Your BMI is %.2f", bmi);
   }
 }
--- Question3.java
+++ Question3.java
@@ -4,13 +4,15 @@ public class Question3
 {
   public static void main(String[] args)
   {
-    /**
-     * Reads integer entered by the user, multiplies the integer by itself and print out the result.
-     * 
-     * Hint: in.nextInt() to read integer
-     */
-     
     Scanner in = new Scanner(System.in);
+
+    System.out.print("Enter your number: ");
+
+    int number = in.nextInt();
+
+    int result = number * number;
+
+    System.out.print(result);
     
   }
 }
