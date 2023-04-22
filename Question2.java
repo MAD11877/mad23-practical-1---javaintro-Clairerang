ccba7be91fa11a2d02a2168d130a7117725753bc
Clairerang <s10223223@connect.np.edu.sg>
2023-04-22 13:01:42 +0800

    test 18

--- Question2.java
+++ Question2.java
@@ -4,18 +4,18 @@ public class Question2
 {
   public static void main(String[] args)
   {
-    Scanner in = new Scanner(System.in);
+    try (Scanner in = new Scanner(System.in)) 
+    {
+      System.out.println("Enter your weight in kg: ");
+      double weight = in.nextDouble();
+          
+      System.out.println("Enter your height in meters: ");
+      double height = in.nextDouble();
+          
+      double bmi = weight / (height * height);
 
-
-    System.out.println("Enter your weight in kg: ");
-    double weight = in.nextDouble();
-        
-    System.out.println("Enter your height in meters: ");
-    double height = in.nextDouble();
-        
-    double bmi = weight / (height * height);
-
-    System.out.println(bmi);
+      System.out.println(bmi);
+    }
 
 
   }
